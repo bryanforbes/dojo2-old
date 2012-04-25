c9de46213dff72bb51a6148fc851ec2cbf896b68
Bryan Forbes <bryan@reigndropsfall.net>
2012-04-25 14:56:23 -0500

    Added setGlobal and withGlobal.

--- env.js
+++ env.js
@@ -1,5 +1,22 @@
-define([], function(){
-	return {
-		global: this
+define(['exports'], function(exports){
+	exports.global = this;
+
+	exports.setGlobal = function(newGlobal){
+		exports.global = newGlobal;
+	};
+
+	exports.withGlobal = function(globalObject, callback, thisObject, cbArguments){
+		var oldGlobal = exports.global;
+		try{
+			exports.setGlobal(globalObject);
+
+			if(thisObject && typeof callback == "string"){
+				callback = thisObject[callback];
+			}
+
+			return callback.apply(thisObject, cbArguments || []);
+		}finally{
+			exports.setGlobal(oldGlobal);
+		}
 	};
 });
