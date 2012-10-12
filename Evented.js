a1e6af0cac1c6332e5f585dc27f4f3528cc95d5c
Bryan Forbes <bryan@reigndropsfall.net>
2012-10-11 19:04:36 -0500

    Use compose to implement Eventd: compose is no longer needed as an explicit dependency to inherit from Evented.

--- Evented.js
+++ Evented.js
@@ -1,25 +1,23 @@
-define(["./aspect", "./on"], function(aspect, on){
-	// module:
+define(["./compose", "./aspect", "./on"], function(compose, aspect, on){
+	//	module:
 	//		dojo/Evented
-	// summary:
-	//		The export of this module is a class that can be used as a mixin or base class, 
-	// 		to add on() and emit() methods to a class
-	// 		for listening for events and emitting events:
+	//	summary:
+	//		The export of this module is a class that can be used as a mixin or base class,
+	//		to add on() and emit() methods to a class
+	//		for listening for events and emitting events:
 	//
-	// 		|	define(["dojo/Evented"], function(Evented){
-	// 		|		var EventedWidget = dojo.declare([Evented, dijit._Widget], {...});
+	//		|	define(["dojo/Evented", "dijit/_Widget"], function(Evented, _Widget){
+	//		|		var EventedWidget = Evented.extend(_Widget, {...});
 	//		|		widget = new EventedWidget();
 	//		|		widget.on("open", function(event){
 	//		|		... do something with event
-	//		|	 });
+	//		|	});
 	//		|
 	//		|	widget.emit("open", {name:"some event", ...});
 
- 	"use strict";
- 	var after = aspect.after;
-	function Evented(){
-	}
-	Evented.prototype = {
+	"use strict";
+	var after = aspect.after;
+	return compose({
 		on: function(type, listener){
 			return on.parse(this, type, listener, function(target, type){
 				return after(target, 'on' + type, listener, true);
@@ -30,6 +28,5 @@ define(["./aspect", "./on"], function(aspect, on){
 			args.push.apply(args, arguments);
 			return on.emit.apply(on, args);
 		}
-	};
-	return Evented;
+	});
 });
