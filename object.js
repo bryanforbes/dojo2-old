6d5f1e8e90543bee858764de153c6d8889d57825
Bryan Forbes <bryan@reigndropsfall.net>
2012-10-11 19:08:25 -0500

    Drop defineProperties for simple shims.

--- object.js
+++ object.js
@@ -2,45 +2,34 @@ define(['exports', './has'], function(exports, has){
 	has.add('object-is', typeof Object.is === 'function');
 	has.add('object-isnt', typeof Object.isnt === 'function');
 
-	Object.defineProperties(exports, {
-		is: {
-			value: has('object-is') ?
-				function(x, y){
-					return Object.is(x, y);
-				} :
-				function(x, y){
-					// taken from http://wiki.ecmascript.org/doku.php?id=harmony:egal
-					if(x === y){
-						// 0 === -0, but they aren't identical
-						return x !== 0 || 1 / x === 1 / y;
-					}
+	exports.is = has('object-is') ?
+		function(x, y){
+			return Object.is(x, y);
+		} :
+		function(x, y){
+			// taken from http://wiki.ecmascript.org/doku.php?id=harmony:egal
+			if(x === y){
+				// 0 === -0, but they aren't identical
+				return x !== 0 || 1 / x === 1 / y;
+			}
 
-					// NaN !== NaN, but they are identical
-					// NaNs are the only non-reflexive value, i.e., if x !== x,
-					// then x is a NaN.
-					// isNaN is broken: it converts its argument to number, so
-					// isNaN("foo") => true
-					return x !== x && y !== y;
-				},
-			enumerable: true,
-			configurable: true,
-			writable: true
-		},
-		isnt: {
-			value: has('object-isnt') ?
-				function(x, y){
-					return Object.isnt(x, y);
-				} :
-				has('object-is') ?
-					function(x, y){
-						return !Object.is(x, y);
-					} :
-					function(x, y){
-						return !exports.is(x, y);
-					},
-			enumerable: true,
-			configurable: true,
-			writable: true
-		}
-	});
+			// NaN !== NaN, but they are identical
+			// NaNs are the only non-reflexive value, i.e., if x !== x,
+			// then x is a NaN.
+			// isNaN is broken: it converts its argument to number, so
+			// isNaN("foo") => true
+			return x !== x && y !== y;
+		};
+
+	exports.isnt = has('object-isnt') ?
+		function(x, y){
+			return Object.isnt(x, y);
+		} :
+		has('object-is') ?
+			function(x, y){
+				return !Object.is(x, y);
+			} :
+			function(x, y){
+				return !exports.is(x, y);
+			};
 });
--- string.js
+++ string.js
@@ -7,73 +7,46 @@ define(['exports', './has'], function(exports, has){
 	has.add('string-repeat', typeof sp.repeat === 'function');
 	has.add('string-toarray', typeof sp.toArray === 'function');
 
-	Object.defineProperties(exports, {
-		startsWith: {
-			value: has('string-startswith') ?
-				function(string, search){
-					return string.startsWith(search);
-				} :
-				function(string, search){
-					return !string.indexOf(search);
-				},
-			enumerable: true,
-			configurable: true,
-			writable: true
-		},
-		endsWith: {
-			value: has('string-endswith') ?
-				function(string, search){
-					return string.endsWith(search);
-				} :
-				function(string, search){
-					search = search + '';
-					var i = string.lastIndexOf(search);
-					return i >= 0 && i === (string.length - search.length);
-				},
-			enumerable: true,
-			configurable: true,
-			writable: true
-		},
-		contains: {
-			value: has('string-contains') ?
-				function(string, search){
-					return string.contains(search);
-				} :
-				function(string, search){
-					return string.indexOf(search) !== -1;
-				},
-			enumerable: true,
-			configurable: true,
-			writable: true
-		},
-		repeat: {
-			value: has('string-repeat') ?
-				function(string, count){
-					return string.repeat(count);
-				} :
-				function(string, count){
-					string = string + '';
-					var result = '';
-					while(--count >= 0){
-						result += string;
-					}
-					return result;
-				},
-			enumerable: true,
-			configurable: true,
-			writable: true
-		},
-		toArray: {
-			value: has('string-toarray') ?
-				function(string){
-					return string.toArray();
-				} :
-				function(string){
-					return string.split('');
-				},
-			enumerable: true,
-			configurable: true,
-			writable: true
-		}
-	});
+	exports.startsWith = has('string-startswith') ?
+		function(string, search){
+			return string.startsWith(search);
+		} :
+		function(string, search){
+			return !string.indexOf(search);
+		};
+	exports.endsWith = has('string-endswith') ?
+		function(string, search){
+			return string.endsWith(search);
+		} :
+		function(string, search){
+			search = search + '';
+			var i = string.lastIndexOf(search);
+			return i >= 0 && i === (string.length - search.length);
+		};
+	exports.contains = has('string-contains') ?
+		function(string, search){
+			return string.contains(search);
+		} :
+		function(string, search){
+			return string.indexOf(search) !== -1;
+		};
+	exports.repeat = has('string-repeat') ?
+		function(string, count){
+			return string.repeat(count);
+		} :
+		function(string, count){
+			string = string + '';
+			var result = '';
+			while(--count >= 0){
+				result += string;
+			}
+			return result;
+		};
+	exports.toArray = has('string-toarray') ?
+		function(string){
+			return string.toArray();
+		} :
+		function(string){
+			return string.split('');
+		};
 });
